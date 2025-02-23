a1b33d8534602469714c18245950eafe3e787a5e
om kathar <omkathar15@gmail.com>
2025-02-23 17:49:26 +0530

    final changes

--- client/chatApp/src/components/chat/Chat.jsx
+++ client/chatApp/src/components/chat/Chat.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import ChatApp from "../chatapp/ChatApp";
 import Profile from "../profile/Profile";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:3000");
 
 function Chat() {
   return (
--- client/chatApp/src/components/registration/signin/Signin.jsx
+++ client/chatApp/src/components/registration/signin/Signin.jsx
@@ -16,7 +16,7 @@ function Signin() {
   const handleLogin = async () => {
     try {
       
-      const response = await axios.post('http://localhost:3000/app/signin', {
+      const response = await axios.post('http://localhost:3000/api/signin', {
         email,
         password
       });
